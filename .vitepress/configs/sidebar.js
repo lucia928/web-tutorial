0190d5664e76de20dc3072d664f5bc33419b737c
lucia928 <luxuhui1998@163.com>
2025-04-08 21:50:47 +0800

    updata: basic

--- .vitepress/configs/sidebar.js
+++ .vitepress/configs/sidebar.js
@@ -2,50 +2,57 @@
  * @Author: lucia928
  * @Date: 2025-03-30 16:25:06
  * @LastEditors: lucia928
- * @LastEditTime: 2025-04-01 23:10:23
+ * @LastEditTime: 2025-04-08 21:48:32
  * @FilePath: \web-tutorial\.vitepress\configs\sidebar.js
- * @Description:  
+ * @Description:
  * @Copyright (c) 2025 by luxuhui1998@163.com, All Rights Reserved.
  */
 export default {
-  '/docs/basic/': getBasicSideBar(),
-  '/docs/css/': getCssSideBar(),
+  "/docs/basic/": getBasicSideBar(),
+  "/docs/css/": getCssSideBar(),
 }
 
 function getBasicSideBar() {
   return [
     {
-      text: 'JS基础',
+      text: "JS基础",
       collapsed: false,
       items: [
         {
           text: "动态类型语言",
-          link: "/docs/basic/dynamic.md"
+          link: "/docs/basic/dynamic.md",
         },
         {
           text: "数据类型",
-          link: "/docs/basic/type.md"
+          link: "/docs/basic/type.md",
         },
         {
           text: "变量声明",
-          link: "/docs/basic/define.md"
-        }
+          link: "/docs/basic/define.md",
+        },
+        {
+          text: "类型转换",
+          link: "/docs/basic/transform.md",
+        },
+        {
+          text: "==和===",
+          link: "/docs/basic/equal.md",
+        },
       ],
-    }
+    },
   ]
 }
-
 
 function getCssSideBar() {
   return [
     {
-      text: 'CSS系列',
+      text: "CSS系列",
       items: [
         {
           text: "盒子模型",
-          link: "/docs/css/box.md"
-        }
+          link: "/docs/css/box.md",
+        },
       ],
-    }
+    },
   ]
 }
---
+++ docs/basic/equal.md
@@ -0,0 +1,95 @@
+# ==和===的区别
+
+## 等于操作符（==）
+
+会先触发隐式类型转换，再比较值
+
+``` javascript
+5 == '5'; // true 先把'5'转为5
+
+true === 1; // true 先把true转为1
+
+[] == 0; // true 先把[]转为''再转为0
+
+let obj = {
+    valueOf: function() {
+        return 1;
+    }
+}
+obj == 1; // true 先调用对象的valueOf方法取得原始值再进行比较
+
+null == undefined; // true 特殊规则
+
+let obj1 = {name:"xxx"};
+let obj2 = obj1;
+obj1 == obj2; // true obj1、obj2指向同一内存地址
+
+NaN == NaN; // false 
+
+{} == {}; // false 内存地址不一样
+```
+
+
+
+一些违反直觉的结果
+
+``` javascript
+0 == '' // true
+0 == '0' // true
+'' == '0' // false
+
+false == 'false' // false
+false == '0' // true
+
+false == undefined // false
+false == null // false
+null == undefined // true
+
+' \t\r\n' == 0 // true
+```
+
+
+
+### 规则
+
+- 两个都为基本类型，字符串和布尔值都会转换成数值，再比较
+
+- 基本类型与引用类型比较，对象转化成其原始类型的值，再比较
+
+- 两个都为引用类型，则比较它们是否指向同内存地址
+
+- null 和 undefined 相等
+
+- 存在 NaN 则返回 false
+
+
+
+## 全等操作符（===）
+
+直接比较值和类型，不触发类型转换，类型不同直接返回`false`
+
+``` javascript
+5 == '5'; // false 类型不同
+
+true === 1; //  false 类型不同
+
+[] == 0; //  false 类型不同
+
+let obj = {
+    valueOf: function() {
+        return 1;
+    }
+}
+obj == 1; // false 类型不同
+
+null == undefined; //  false 类型不同
+
+NaN == NaN; // false 
+
+let obj1 = {name:"xxx"};
+let obj2 = obj1;
+obj1 === obj2; // true obj1、obj2指向同一内存地址
+```
+
+
+
